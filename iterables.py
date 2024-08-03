7a8ca63e57c298882358c0a37b95777e68ae6180
Leandro Lopes <lopes.ctrlf5@gmail.com>
2024-08-03 19:03:16 -0300

    Add files via upload

---
+++ iterables.py
@@ -0,0 +1,30 @@
+# Estrutura Range()
+# range(tamanho)
+# ou
+# range(inicio:fim)
+# ou
+# range(inicio:fim:hertz)
+
+
+#Estrutura Set()
+#meu_set = {'valor', 'valor', 'valor'}
+# *exclui valores repetidos
+# *não possui ordem
+
+# adicionar item no set
+# meu_set.add('airpod')
+
+# remover item do set
+# meu_set.remove('item')
+
+# une itens de dois sets (sem repeti-los)
+# todos_produtos = produtos.union(lancamentos)
+
+# limpar todo o set
+# meu_set.clear()
+
+# pega apenas informações que existem em dois sets ao mesmo tempo
+# intersecao = produtos.intersection(lancamentos)
+
+# retorna apenas informações de um set que não existem em outro set
+# produtos_nao_lancamentos = produtos.difference(lancamentos)
